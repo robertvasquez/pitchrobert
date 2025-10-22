7ffad2a2b0b808d45bbfff8bc41ed32a08a18691
robertvasquez <robertvasquez89@gmail.com>
2025-10-22 14:05:24 -0500

    refactor: slides
    refactor: hardcode list of slide images

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,21 @@
 import type { PageServerLoad } from './$types';
 
-const modules = import.meta.glob('/static/slides/*.{png,jpg,jpeg,gif,webp,avif}', { eager: true });
+// Hard-coded list of slide images that start with "Slide" in the static root.
+// Using absolute paths so they resolve correctly from the /static directory at runtime.
+const slides = [
+    '/SlideA1.PNG',
+    '/SlideB2.PNG',
+    '/SlideC3.PNG',
+    '/SlideD4.PNG',
+    '/SlideE5.PNG',
+    '/SlideF6.PNG',
+    '/SlideG7.PNG',
+    '/SlideH8.PNG',
+    '/SlideI9.PNG',
+    '/SlideJ10.PNG',
+    '/SlideK11.PNG'
+];
 
 export const load: PageServerLoad = async () => {
-    const slides = Object.keys(modules)
-        .map((path) => path.replace('/static', ''))
-        .sort();
     return { slides };
 };
